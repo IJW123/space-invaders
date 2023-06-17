fcd2a8e47da09f0636269f8f28265a55a0b4d768
Isaak <IJW123@users.noreply.github.com>
2023-06-17 20:14:10 +0200

    Black window

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .insert_resource(ClearColor(Color::rgb(0.04, 0.04, 0.04)))
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+            title: "Rust Invaders!".into(),
+            resolution: (598., 676.).into(),
+            ..Default::default()
+        }),
+        ..Default::default()
+    }))
+    .run();
+}
+  
+fn setup_system(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+}
