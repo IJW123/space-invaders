f5e97d362c0ed4a4f87405ae1201ea544aee1dd3
Isaak <IJW123@users.noreply.github.com>
2024-06-24 21:15:14 +0200

    Added border and laser movement and despawn

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
-use bevy::prelude::*;
+use bevy::{
+    prelude::*,
+    sprite::{MaterialMesh2dBundle, Mesh2dHandle},
+};
+
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
 use player::PlayerPlugin;
 
-mod player;
 mod components;
+mod player;
 
+const SCREEN_SIZE_X: f32 = 900.;
+const SCREEN_SIZE_Y: f32 = 1200.;
 const PLAYER_SPRITE: &str = "player_icon_turtle_01.png";
 const PLAYER_LASER_SPRITE: &str = "laser_a_01.png";
 const PLAYER_SIZE: (f32, f32) = (92., 92.);
-const PLAYER_LASER_SIZE: (f32, f32) = (9., 54.);
+// const PLAYER_LASER_SIZE: (f32, f32) = (9., 54.);
 const SPRITE_SCALE: f32 = 0.25;
 
 #[derive(Resource)]
@@ -29,34 +35,35 @@ fn main() {
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             primary_window: Some(Window {
                 title: "Rust Invaders".into(),
-                resolution: (900.,1200.).into(),
+                resolution: (SCREEN_SIZE_X, SCREEN_SIZE_Y).into(),
                 ..Default::default()
             }),
             ..Default::default()
         }))
         .add_plugins(PlayerPlugin)
         .add_plugins(WorldInspectorPlugin::new())
-
         .add_systems(Startup, setup_system)
         .run()
 }
 
 fn setup_system(
-    mut commands: Commands, 
+    mut commands: Commands,
     asset_server: Res<AssetServer>,
     query: Query<&Window>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
     // camera
     commands.spawn(Camera2dBundle::default());
 
     // Window size
     let Ok(primary) = query.get_single() else {
-		return;
-	};
-	let (win_w, win_h) = (primary.width(), primary.height());
+        return;
+    };
+    let (win_w, win_h) = (primary.width(), primary.height());
 
     // add WinSize resource
-    let win_size = WinSize {w: win_w, h: win_h};
+    let win_size = WinSize { w: win_w, h: win_h };
     commands.insert_resource(win_size);
 
     // add GameTextures resource
@@ -65,7 +72,21 @@ fn setup_system(
         player_laser: asset_server.load(PLAYER_LASER_SPRITE),
     };
     commands.insert_resource(game_textures);
-       
+
+    let rectangle_outer = Mesh2dHandle(meshes.add(Rectangle::new(SCREEN_SIZE_X, SCREEN_SIZE_Y)));
+    let rectangle_inner =
+        Mesh2dHandle(meshes.add(Rectangle::new(SCREEN_SIZE_X - 10., SCREEN_SIZE_Y - 10.)));
+
+    commands.spawn(MaterialMesh2dBundle {
+        mesh: rectangle_outer,
+        material: materials.add(Color::rgb_u8(255, 255, 255)),
+        transform: Transform::from_xyz(0.0, 0.0, -21.), // Slightly forward in Z
+        ..default()
+    });
+    commands.spawn(MaterialMesh2dBundle {
+        mesh: rectangle_inner,
+        material: materials.add(Color::rgb_u8(0, 0, 0)),
+        transform: Transform::from_xyz(0.0, 0.0, -20.), // Even more forward in Z
+        ..default()
+    });
 }
-
-
--- src/player.rs
+++ src/player.rs
@@ -1,27 +1,32 @@
-use crate::{components::{Player, Velocity}, GameTextures, WinSize, PLAYER_SIZE, SPRITE_SCALE};
-use bevy::{ecs::query, input::{keyboard::Key, InputPlugin}, prelude::*};
+use crate::{
+    components::{Laser, Player, Velocity},
+    GameTextures, WinSize, PLAYER_SIZE, SCREEN_SIZE_Y, SPRITE_SCALE,
+};
+use bevy::prelude::*;
 
-const TIME_STEP:  f32 = 1. / 60.;
+const TIME_STEP: f32 = 1. / 60.;
 const BASE_SPEED: f32 = 500.;
-const MIN_X:      f32 = -450. + (PLAYER_SIZE.0 / 2.);
-const MAX_X:      f32 = 450. - (PLAYER_SIZE.0 / 2.);
-const MIN_Y:      f32 = -600. + (PLAYER_SIZE.1 / 2.);
-const MAX_Y:      f32 = 600. - (PLAYER_SIZE.1 / 2.);
+const PLAYER_MIN_X: f32 = -450. + (PLAYER_SIZE.0 / 2.);
+const PLAYER_MAX_X: f32 = 450. - (PLAYER_SIZE.0 / 2.);
+const PLAYER_MIN_Y: f32 = -600. + (PLAYER_SIZE.1 / 2.);
+const PLAYER_MAX_Y: f32 = 600. - (PLAYER_SIZE.1 / 2.);
+const LASER_VELOCITY_X: f32 = 0.;
+const LASER_VELOCITY_Y: f32 = 0.2;
 
 pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
-        app
-            .add_systems(PostStartup, player_spawn_system)
+        app.add_systems(PostStartup, player_spawn_system)
             .add_systems(Update, player_movement_system)
             .add_systems(Update, player_keyboard_event_system)
-            .add_systems(Update, player_fire_system);
+            .add_systems(Update, player_fire_system)
+            .add_systems(Update, laser_movement_system);
     }
 }
 
 fn player_spawn_system(
-    mut commands: Commands, 
+    mut commands: Commands,
     game_textures: Res<GameTextures>,
     win_size: Res<WinSize>,
 ) {
@@ -30,42 +35,51 @@ fn player_spawn_system(
         .spawn(SpriteBundle {
             texture: game_textures.player.clone(),
             transform: Transform {
-                translation: Vec3::new(0.0,bottom + PLAYER_SIZE.1 / 2. + 5. , 10.),
+                translation: Vec3::new(0.0, bottom + PLAYER_SIZE.1 / 2. + 5., 10.),
                 scale: Vec3::new(SPRITE_SCALE, SPRITE_SCALE, 1.),
                 ..Default::default()
             },
             ..Default::default()
         })
+        .insert(Name::new("player"))
         .insert(Player)
-        .insert(Velocity {x: 0., y: 0.});
+        .insert(Velocity { x: 0., y: 0. });
 }
 
-fn player_fire_system (
+fn player_fire_system(
     mut commands: Commands,
     kb: Res<ButtonInput<KeyCode>>,
     game_textures: Res<GameTextures>,
-    query: Query<&Transform, With<Player>>, 
-){
+    query: Query<&Transform, With<Player>>,
+) {
     if let Ok(player_tf) = query.get_single() {
         if kb.just_pressed(KeyCode::Space) {
             let (x, y) = (player_tf.translation.x, player_tf.translation.y);
 
-            commands.spawn(SpriteBundle {
-                texture: game_textures.player_laser.clone(),
-                transform: Transform {
-                    translation: Vec3::new(x, y, 0.),
-                    scale: Vec3::new(SPRITE_SCALE, SPRITE_SCALE, 1.),
+            commands.spawn((
+                SpriteBundle {
+                    texture: game_textures.player_laser.clone(),
+                    transform: Transform {
+                        translation: Vec3::new(x, y + PLAYER_SIZE.1 / 2., 0.),
+                        scale: Vec3::new(SPRITE_SCALE, SPRITE_SCALE, 1.),
+                        ..Default::default()
+                    },
                     ..Default::default()
                 },
-                ..Default::default()
-            });
+                Laser,
+                Velocity {
+                    x: LASER_VELOCITY_X,
+                    y: LASER_VELOCITY_Y,
+                },
+                Name::new("laser"),
+            ));
         }
     }
 }
 
 fn player_keyboard_event_system(
     kb: Res<ButtonInput<KeyCode>>,
-    mut query: Query<&mut Velocity, With<Player>>
+    mut query: Query<&mut Velocity, With<Player>>,
 ) {
     if let Ok(mut velocity) = query.get_single_mut() {
         velocity.x = if kb.pressed(KeyCode::KeyA) {
@@ -81,28 +95,29 @@ fn player_keyboard_event_system(
             -1.
         } else {
             0.
-        };    
+        };
     }
 }
 
-fn player_movement_system(
-    mut query: Query<(&Velocity, &mut Transform), With<Player>>
-) {
+fn player_movement_system(mut query: Query<(&Velocity, &mut Transform), With<Player>>) {
     for (velocity, mut transform) in query.iter_mut() {
-        // let translation = &mut transform.translation;
         transform.translation.x += velocity.x * BASE_SPEED * TIME_STEP;
         transform.translation.y += velocity.y * BASE_SPEED * TIME_STEP;
-        if transform.translation.x < MIN_X {
-            transform.translation.x = MIN_X
-        }
-        if transform.translation.x > MAX_X {
-            transform.translation.x = MAX_X
-        }
-        if transform.translation.y < MIN_Y {
-            transform.translation.y = MIN_Y
-        }
-        if transform.translation.y > MAX_Y {
-            transform.translation.y = MAX_Y
+        transform.translation.x = transform.translation.x.clamp(PLAYER_MIN_X, PLAYER_MAX_X);
+        transform.translation.y = transform.translation.y.clamp(PLAYER_MIN_Y, PLAYER_MAX_Y);
+    }
+}
+
+fn laser_movement_system(
+    mut query: Query<(Entity, &Velocity, &mut Transform), With<Laser>>,
+    mut commands: Commands,
+) {
+    for (entity, velocity, mut transform) in query.iter_mut() {
+        transform.translation.x += velocity.x * BASE_SPEED * TIME_STEP;
+        transform.translation.y += velocity.y * BASE_SPEED * TIME_STEP;
+
+        if transform.translation.y > SCREEN_SIZE_Y / 2. {
+            commands.entity(entity).despawn();
         }
     }
 }
